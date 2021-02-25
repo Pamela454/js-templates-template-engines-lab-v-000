4625776bf1f1085fd570404581d8408d397da40d
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-02-25 12:03:51 -0500

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function createPost() {
 	let page = getElementById("page-template").innerHTML
     let post = getElementById("post-form").value
 
+
+    let pageTemplate = '<div id="page-template"><%= page %><span class="page"></span><%= page %></div>';
+
     let postTemplate = '<div id="post-template"><%= post %><article><header></header><footer></footer></article><span class="post"></span><%= post %></div>';
 
     let templateFn = _.template(postTemplate);
