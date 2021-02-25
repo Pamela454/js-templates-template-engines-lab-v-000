866d0a78aad07eecab1c2590a2939df9159116c4
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-02-25 12:01:10 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function createPost() {
+	let page = getElementById("page-template").innerHTML
+    let post = getElementById("post-form").value
+
+    let postTemplate = '<div id="post-template"><%= post %><header></header><article></article><footer></footer><span class="post"></span><%= post %></div>';
+
+    let templateFn = _.template(postTemplate);
+
+    let postDiv = document.getElementById('post-template');
+
+    let templateHTML = templateFn({ post: post });
+
+    postDiv.innerHTML += templateHTML;
+}
+
+function postComment() {
+	let comment = getElementById("comment-form").value
+
+	let commentTemplate = '<div id="comment-template"><p><%= comment %><span class="comment"><%= comment %></span></p></div>';
+
+}
